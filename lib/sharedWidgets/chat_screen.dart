dcaf9ccda355c077018952a4010d235af8c18fa7
Timnde Rahwan <timnderahwan@Timndes-MacBook-Pro.local>
2022-09-07 16:15:32 +0300

    update 15

--- lib/sharedWidgets/chat_screen.dart
+++ lib/sharedWidgets/chat_screen.dart
@@ -9,7 +9,6 @@ import 'package:flutter_linkify/flutter_linkify.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:image_editor/image_editor.dart';
 import 'package:kuungaa/AllWidgets/progressDialog.dart';
 import 'package:kuungaa/Assistants/assistantMethods.dart';
 import 'package:kuungaa/DataHandler/appData.dart';
