a8b9b5f0da73a3dfc227942adecb8bc26bb31dde
Peter Samuel <ptahsamuel@gmail.com>
2023-01-12 13:00:13 +0300

    payment update

--- lib/sharedWidgets/kuungaa_chat.dart
+++ lib/sharedWidgets/kuungaa_chat.dart
@@ -34,9 +34,13 @@ class _KuungaaChatState extends State<KuungaaChat> {
   @override
   void initState() {
     // TODO: implement initState
+    if(widget.receivedAction!.payload?['chatid'] != null && widget.receivedAction!.payload?['chatid'] != ""){
+      navigateToChat(widget.receivedAction!.payload?['chatid']);
+    }
     super.initState();
     AssistantMethods.getChats(context);
     AssistantMethods.getChatUsers(context);
+
   }
   @override
   Widget build(BuildContext context) {
@@ -470,6 +474,39 @@ class _KuungaaChatState extends State<KuungaaChat> {
       }
     });
     return userChatList.reversed.toList();
+  }
+
+  Future<Chat> getChatFromID(String chatid) async{
+    Chat chat = Chat();
+    DatabaseReference userChatRef = FirebaseDatabase.instance.reference().child("KUUNGAA").child("Chats").child(chatid);
+    await userChatRef.once().then((DataSnapshot dataSnapshot) async {
+      if(dataSnapshot.exists){
+        chat.chat_createddate = dataSnapshot.value["chat_createddate"];
+        chat.chat_id = dataSnapshot.value["chat_id"];
+        chat.chat_creatorid = dataSnapshot.value["chat_creatorid"];
+        chat.chat_partnerid = dataSnapshot.value["chat_partnerid"];
+        DatabaseReference memberRef = FirebaseDatabase.instance.reference().child("KUUNGAA").child("Chats").child(chatid).child("members");
+        await memberRef.once().then((DataSnapshot members){
+          var zees = members.value.keys;
+          var data = members.value;
+          for(var zee in zees){
+            if(data[zee]["member_id"] != userCurrentInfo!.user_id!){
+              chat.chat_opponentid = data[zee]["member_id"];
+            }
+          }
+        });
+        chat.opponentUser = await AssistantMethods.getCurrentOnlineUser(chat.chat_opponentid!);
+      }
+    });
+    return chat;
+  }
+
+  void navigateToChat(String? payload) {
+    Future.delayed(Duration.zero,()
+    async{
+      Chat chat = await getChatFromID(payload!);
+      Navigator.push(context, PageTransition(type: PageTransitionType.rightToLeft, child: ChatScreen(chat: chat,)));
+    });
   }
 }
 
